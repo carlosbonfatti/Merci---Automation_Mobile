d7737e4f80fc039f89a736990768743942f32e13
carlosbonfatti <carlos.bonfatti@dock.tech>
2021-10-29 15:37:50 -0300

    teste

--- Merci_automation_mobile/features/support/env.rb
+++ Merci_automation_mobile/features/support/env.rb
@@ -8,6 +8,15 @@ require "pry"
 require "rspec"
 require "selenium-webdriver"
 
+require "fileutils"
+require_relative "./utils/user_types/base_helpers"
+
+include BaseHelpers
+
+# limpa os logs e os scrennshots
+FileUtils.rm_f(Dir.glob("logs/*.json"))
+FileUtils.rm_f(Dir.glob("logs/*.png"))
+
 EL_ANDROID = YAML.load_file(File.join(Dir.pwd, "features/support/config/elements/android/elements_android.yml"))
 
 @device = ENV["DEVICE"]
